2cac7a6d5d76ba6717aa59a8ae3740b11dba47c1
Carlos Araújo <111444349+carlosarauj@users.noreply.github.com>
2024-05-07 19:38:32 -0300

    Add files via upload

---
+++ py.py
@@ -0,0 +1,17 @@
+nome = "Carlos"
+
+for ind in nome:
+    print(ind)
+
+print("index01")
+for index in range(10):
+    print(index)
+
+print("index02")
+for index2 in range(2,10):
+    print(index2)
+
+print("index03")
+for index3 in range(2,10, 2):
+    print(index3)
+#usa o terminal com: python nomedoarquivo.py
